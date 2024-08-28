835ad9f883ef3bbc7868d8db3e4c2d7dd2e5e566
Jiajun Tang <zhiju022486@163.com>
2024-08-28 14:26:18 +0800

    supporting epub

--- Homepage.py
+++ Homepage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 
 st.set_page_config(
@@ -24,3 +25,28 @@ st.markdown(
     
     支持各类历史档案文件（PDF、JPG、PNG等格式）的处理和翻译。通过系统的提示词优化达成最佳效果。
 """)
+
+data_df = pd.DataFrame(
+    {
+        "模型": ["Kimi", "文心一言", "通义千问", "ChatGPT", "Gemini"],
+        "是否可用": ["✓", "✓", "开发中", "×", "×"],
+        "是否免费": ["否", "是", "开发中", "否", "是"],
+        "计算文本Token": ["✓", "✓", "开发中", "×", "×"],
+        "计算图像Token": ["✓", "×", "开发中", "×", "×"],
+        "文本翻译": ["✓", "✓", "开发中", "×", "×"],
+        "图像翻译": ["✓", "×", "开发中", "×", "×"],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "widgets": st.column_config.TextColumn(
+            "Widgets",
+            default="st.",
+            max_chars=50,
+            validate=r"^st\.[a-z_]+$",
+        )
+    },
+    hide_index=True,
+)
--- pages/3档案翻译.py
+++ pages/3档案翻译.py
@@ -1,7 +1,10 @@
 import io
 import os
 import json
+import tempfile
 import pypdfium2
+import ebooklib
+from ebooklib import epub
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 from Authentic import get_api_key, get_authentication
@@ -11,6 +14,7 @@ import requests
 import qianfan
 from qianfan.resources.tools import tokenizer
 import textwrap
+from bs4 import BeautifulSoup
 import re
 # from surya.model.detection.model import load_model, load_processor
 # from surya.model.recognition.model import load_model as load_rec_model
@@ -69,14 +73,12 @@ def save_to_file(file_name, pil_image, page_number):
         os.makedirs(output_dir)
     pil_image.save(output_path, "PNG")
     
-def get_estimate_token(model, api_key, in_file):
+def get_estimate_token_text(model, api_key, in_file):
     if model == "Kimi":
         client = OpenAI(
             api_key = api_key,
             base_url = "https://api.moonshot.cn/v1"
         )
-        file_object = client.files.create(file = in_file, purpose="file-extract")
-        file_content = client.files.content(file_id = file_object.id).text
         headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {api_key}"
@@ -85,7 +87,7 @@ def get_estimate_token(model, api_key, in_file):
     "model": "moonshot-v1-128k",
     "messages": [
         {"role" : "system", "content" : "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你是一位历史研究的专家。"},
-        {"role": "system", "content": file_content,
+        {"role": "system", "content": in_file,
     },
         {"role": "user", "content": "请你为我翻译这个图像中的文本，尽可能使结果文本的排版和图像接近，只返回翻译结果。"}
     ]
@@ -97,17 +99,77 @@ def get_estimate_token(model, api_key, in_file):
 )
         response_data = response.json()
         total_tokens = response_data.get("data", {}).get("total_tokens")
-#     elif model == "文心一言":
-#         os.environ["QIANFAN_AK"] = api_key.get("client_id")
-#         os.environ["QIANFAN_SK"] = api_key.get("secret_id")
-#         text_list=get_ocr(in_file)
-#         total_tokens = tokenizer.Tokenizer().count_tokens(
-#         text="".join(text_list),
-#         mode='remote',
-#         model="ernie-speed-128k"
-# )  
+    elif model == "文心一言":
+        os.environ["QIANFAN_AK"] = api_key.get("client_id")
+        os.environ["QIANFAN_SK"] = api_key.get("secret_id")
+        total_tokens = tokenizer.Tokenizer().count_tokens(
+        text=texts,
+        mode='remote',
+        model="ernie-speed-128k"
+)  
     return total_tokens
-        
+
+def get_estimate_token(model, api_key, in_file):
+    if model == "Kimi":
+        headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+        data = {
+    "model": "moonshot-v1-128k",
+    "messages": [
+        {"role" : "system", "content" : "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你是一位历史研究的专家。"},
+        {"role": "system", "content": in_file,
+    },
+        {"role": "user", "content": "请你为我翻译这个图像中的文本，尽可能使结果文本的排版和图像接近，只返回翻译结果。"}
+    ]
+}
+        response = requests.post(
+    "https://api.moonshot.cn/v1/tokenizers/estimate-token-count",
+        headers=headers,
+        data=json.dumps(data)
+)
+        response_data = response.json()
+        total_tokens = response_data.get("data", {}).get("total_tokens")
+    elif model == "文心一言":
+        os.environ["QIANFAN_AK"] = api_key.get("client_id")
+        os.environ["QIANFAN_SK"] = api_key.get("secret_id")
+        total_tokens = tokenizer.Tokenizer().count_tokens(
+        text=in_file,
+        mode='remote',
+        model="ernie-speed-128k"
+)  
+    return total_tokens
+
+def get_chat_completion_text(model, api_key, in_file, prompt):
+    if model == "Kimi":
+        client = OpenAI(
+            api_key = api_key,
+            base_url = "https://api.moonshot.cn/v1"
+        )
+        messages = [
+        {"role" : "system", "content" : "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你是一位历史研究的专家。"},
+        {"role": "system", "content": in_file,
+    },
+        {"role": "user", "content": f"{prompt}。判断翻译后的最后一句话是否完整，若完整则Completion为1，不完整则为0。将结果以json格式返回，格式为'Content': '此处为翻译后文本', 'Completion':'此处为判断是否完整参数'"}
+    ]
+        completion = client.chat.completions.create(
+    model = "moonshot-v1-128k",
+    messages = messages,
+    response_format={"type": "json_object"},
+    temperature = 0
+    )
+        return completion.choices[0].message.content
+    elif model == "文心一言":
+        os.environ["QIANFAN_AK"] = api_key.get("client_id")
+        os.environ["QIANFAN_SK"] = api_key.get("secret_id")
+        chat_comp = qianfan.ChatCompletion()
+        resp = chat_comp.do(model="ERNIE-Speed-128K",system = "你是一位精通中英等语言的专家，也是一位历史档案研究者，出了结果不要返回其他任何东西", messages=[{"role": "user",
+                                                                "content": f"{prompt}。判断翻译后的最后一句话是否完整，若完整则Completion为1，不完整则为0。将结果以json格式返回，格式为'Content': '此处为翻译后文本', 'Completion':'此处为判断是否完整参数'。内容如下: {in_file}"}],
+                            temperature = 0.1)
+        result = re.search(r"\{(.*?)\}", resp["body"]["result"]).group(0)
+        return result
+    
 def get_chat_completion(model, api_key, in_file, prompt):
     if model == "Kimi":
         client = OpenAI(
@@ -129,16 +191,6 @@ def get_chat_completion(model, api_key, in_file, prompt):
     temperature = 0
     )
         return completion.choices[0].message.content
-    # elif model == "文心一言":
-    #     os.environ["QIANFAN_AK"] = api_key.get("client_id")
-    #     os.environ["QIANFAN_SK"] = api_key.get("secret_id")
-    #     content = get_ocr(in_file)
-    #     chat_comp = qianfan.ChatCompletion()
-    #     resp = chat_comp.do(model="ERNIE-Speed-128K",system = "你是一位精通中英等语言的专家，也是一位历史档案研究者，出了结果不要返回其他任何东西", messages=[{"role": "user",
-    #                                                             "content": f"{prompt}。判断翻译后的最后一句话是否完整，若完整则Completion为1，不完整则为0。将结果以json格式返回，格式为'Content': '此处为翻译后文本', 'Completion':'此处为判断是否完整参数'。内容如下: {content}"}],
-    #                         temperature = 0.1)
-    #     result = re.search(r"\{(.*?)\}", resp["body"]["result"]).group(0)
-    #     return result
     
 def make_picture(translated_content, file_name, page_number):
     width, height = 568, 876
@@ -159,11 +211,18 @@ def make_picture(translated_content, file_name, page_number):
         draw.text((x, y), line, font=font, fill=text_color)
         y = y + text_height + 10
     image.save(f"./cached_images/{file_name}/{file_name}_{page_number}_translated.png")
-    
+
+def chapter_to_markdown(chapter):
+    soup = BeautifulSoup(chapter.get_body_content(), 'html.parser')
+    h1_tags = soup.find_all('h1')
+    h1_text = '\n'.join([f'# {tag.get_text()}' for tag in h1_tags])
+    p_tags = soup.find_all('p')
+    p_text = '\n'.join([f'{tag.get_text()}' for tag in p_tags])
+    return f'{h1_text}\n{p_text}'
+
 def save_prompt(new_prompt):
     new_prompt_str = str(new_prompt)
     file_path = "./static/prompts.txt"
-
     try:
         with open(file_path, "r+", encoding="utf-8") as file:
             prompts = json.load(file)
@@ -179,7 +238,8 @@ def save_prompt(new_prompt):
     
     with open(file_path, "w", encoding="utf-8") as file:
         json.dump(prompts, file, ensure_ascii=False)
-    
+
+  
 with open("./static/prompts.txt", "r", encoding="utf-8") as f:
     prompts = f.read()
     prompts = json.loads(prompts)
@@ -195,8 +255,8 @@ def update_api_key(model):
 st.sidebar.title("历史档案翻译")
 in_model = st.sidebar.selectbox(
     "请选择需要使用的模型",
-    options=["ChatGPT", "Kimi", "Gemini", "文心一言"],
-    help="ChatGPT和Gemini模型暂时不可用，使用文心一言时请勿连接VPN"
+    options=["Kimi", "ChatGPT", "Gemini", "文心一言"],
+    help="文心一言、ChatGPT和Gemini模型暂时不可用"
 )
 
 if 'api_key' not in st.session_state:
@@ -216,7 +276,7 @@ translation_button = st.sidebar.button("开始翻译")
 # 右侧主界面
 st.logo("./static/logo.png")
 
-in_file = st.file_uploader("请选择需要处理的历史档案文件")
+in_file = st.file_uploader("请选择需要处理的历史档案文件", type=["pdf", "png", "jpg", "epub"])
 if in_file is None:
     st.stop()
 else:
@@ -228,6 +288,21 @@ if "pdf" in filetype:
     page_count = page_count(in_file)
     page_number = st.number_input(f"选择或输入PDF页码，最大为{page_count}：", min_value=1, value=1, max_value=page_count)
     pil_image = get_page_image(in_file, page_number)
+elif "epub" in filetype:
+    temp_dir = tempfile.mkdtemp()
+    path = os.path.join(temp_dir, in_file.name)
+    with open(path, "wb") as f:
+            f.write(in_file.getvalue())
+    book = epub.read_epub(path)
+    page_count = 0
+    epub_content = []
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            page_count += 1
+            epub_content.append(item)
+    page_number = st.number_input(f"选择或输入EPUB页码，最大为{page_count}：", min_value=1, value=1, max_value=page_count)
+    texts = chapter_to_markdown(epub_content[page_number])
+    pil_image = 0
 else:
     pil_image = Image.open(in_file).convert("RGB")
 
@@ -243,25 +318,38 @@ if pil_image is None:
 
 col1, col2 = st.columns([.5, .5])
 with col1:
-    st.image(pil_image, caption="待处理图像", use_column_width=True)
+    if "epub" in filetype:
+        st.markdown(texts)
+    else:
+        st.image(pil_image, caption="待处理图像", use_column_width=True)
 
 if calculation_button:
-    save_to_file(file_name, pil_image, page_number)
-    image_file = Path(f"./cached_images/{file_name}/{file_name}_{page_number}.png")
-    estimate_token = get_estimate_token(in_model, in_api_key, image_file)
+    if pil_image == 0:
+        estimate_token = get_estimate_token(in_model, in_api_key, texts)
+    else:
+        save_to_file(file_name, pil_image, page_number)
+        image_file = Path(f"./cached_images/{file_name}/{file_name}_{page_number}.png")
+        estimate_token = get_estimate_token(in_model, in_api_key, image_file)
     token_container = st.info(f"翻译该页面预计需要消耗 {estimate_token} token。")
     
 if translation_button:
-    save_to_file(file_name, pil_image, page_number)
-    image_file = Path(f"./cached_images/{file_name}/{file_name}_{page_number}.png")
-    translated_content = get_chat_completion(in_model, in_api_key, image_file, prompt_text)
-    translated_content = json.loads(translated_content)
-    translated_content = translated_content.get("Content")
-    with col2:
-        make_picture(translated_content, file_name, page_number)
-        translated_image = Path(f"./cached_images/{file_name}/{file_name}_{page_number}_translated.png")
-        translated_image = Image.open(translated_image).convert("RGB")
-        st.image(translated_image, caption="处理后图像", use_column_width=True)
-    st.markdown("---")
-    title_t2 = st.subheader("翻译文本")
-    display_content = st.markdown(translated_content)
+    if pil_image == 0:
+        translated_content = get_chat_completion_text(in_model, in_api_key, texts, prompt_text)
+        translated_content = json.loads(translated_content)
+        translated_content = translated_content.get("Content")
+        with col2:
+            display_content = st.markdown(translated_content)
+    else:
+        save_to_file(file_name, pil_image, page_number)
+        image_file = Path(f"./cached_images/{file_name}/{file_name}_{page_number}.png")
+        translated_content = get_chat_completion(in_model, in_api_key, image_file, prompt_text)
+        translated_content = json.loads(translated_content)
+        translated_content = translated_content.get("Content")
+        with col2:
+            make_picture(translated_content, file_name, page_number)
+            translated_image = Path(f"./cached_images/{file_name}/{file_name}_{page_number}_translated.png")
+            translated_image = Image.open(translated_image).convert("RGB")
+            st.image(translated_image, caption="处理后图像", use_column_width=True)
+        st.markdown("---")
+        title_t2 = st.subheader("翻译文本")
+        display_content = st.markdown(translated_content)
